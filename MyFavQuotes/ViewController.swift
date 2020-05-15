8cb64c0bc5bb01a37e1149692a33efd45956cb1a
Stodev <anthony.custodio@edu.itescia.fr>
2020-05-15 12:02:35 +0200

    Initial Commit

---
+++ MyFavQuotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFavQuotes
+//
+//  Created by Custodio Anthony on 15/05/2020.
+//  Copyright © 2020 Stod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
