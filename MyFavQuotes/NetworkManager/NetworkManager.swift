bbd7163e78cca7dd58fd65ccb9d4db0931b23397
Stodev <anthony.custodio@edu.itescia.fr>
2020-05-18 00:30:07 +0200

    Template of NetworkManager - STEP 4

--- MyFavQuotes/NetworkManager/NetworkManager.swift
+++ MyFavQuotes/NetworkManager/NetworkManager.swift
@@ -14,62 +14,15 @@ public class NetworkManager {
     
     
     
-    //    public static func postCreateSessionUser(credentialsJSON: JSON, listener: NetwortProtocol) {
-    //        let url = NSMutableURLRequest(url: NSURL(string: NetworkConst.base_url)! as URL)
-    //        
-    //        url.setValue(NetworkConst.apikey, forHTTPHeaderField: "Authorization")
-    //        
-    //        AF.request(url as! URLConvertible, method: .post).responseJSON { response in
-    //            let jsonResponse = response.result.value {
-    //                listener.handleJSON(json: JSON(jsonResponse))
-    //            }
-    //            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
-    //                print("Data: \(utf8Text)")
-    //            }
-    //        }
-    //    }
-    //    
-    //    public static func getUser() {
-    //        let url = NSMutableURLRequest(url: NSURL(string: NetworkConst.base_url)! as URL)
-    //        
-    //        url.setValue(NetworkConst.apikey, forHTTPHeaderField: "Authorization")
-    //        
-    //        AF.request(url as! URLConvertible, method: .get).responseJSON { response in
-    //            let jsonResponse = response.value {
-    //                listener.handleJSON(json: JSON(jsonResponse))
-    //            }
-    //            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
-    //                print("Data: \(utf8Text)")
-    //            }
-    //        }
-    //    }
-    //    
-    //    
-    //    
-    //    public static func getUserInfo() {
-    //        let url = NSMutableURLRequest(url: NSURL(string: NetworkConst.base_url)! as URL)
-    //        
-    //        url.setValue(NetworkConst.apikey, forHTTPHeaderField: "Authorization")
-    //        AF.request(url as! URLConvertible, method: .get).responseJSON { response in
-    //            let jsonResponse = response.value {
-    //                listener.handleJSON(json: JSON(jsonResponse))
-    //            }
-    //            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
-    //                print("Data: \(utf8Text)")
-    //            }
-    //        }
-    //    }
-    //    
-    public static func getFavQuotesbyUser(pseudo: String, listener: QuotesByUserNetwortProtocol) {
+    public static func postCreateSessionUser(credentialsJSON: JSON, listener: NetwortProtocol) {
         let headers: HTTPHeaders = [
             "Authorization": "Bearer " + NetworkConst.apikey,
             "Accept": "application/json"
         ]
         
         var url = NetworkConst.base_url
-        url = url + "quotes/?filter=" + pseudo + "&type=user"
-        //let URLRequest = NSMutableURLRequest(url: NSURL(string: url)! as URL)
-        AF.request(url as URLConvertible, method: .get, encoding: JSONEncoding.default, headers: headers).responseJSON { response in
+        url = url + "users/:login" //+ credentialsJSON
+        AF.request(url as URLConvertible, method: .post, encoding: JSONEncoding.default, headers: headers).responseJSON { response in
             switch response.result {
             case let .success(value):
                 
@@ -80,4 +33,61 @@ public class NetworkManager {
             }
         }
     }
+        //
+        //    public static func getUser() {
+        //        let url = NSMutableURLRequest(url: NSURL(string: NetworkConst.base_url)! as URL)
+        //
+        //        url.setValue(NetworkConst.apikey, forHTTPHeaderField: "Authorization")
+        //
+        //        AF.request(url as! URLConvertible, method: .get).responseJSON { response in
+        //            let jsonResponse = response.value {
+        //                listener.handleJSON(json: JSON(jsonResponse))
+        //            }
+        //            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+        //                print("Data: \(utf8Text)")
+        //            }
+        //        }
+        //    }
+        //
+        //
+        //
+        public static func getUserInfo(pseudo: String, listener: QuotesByUserNetwortProtocol) {
+            let headers: HTTPHeaders = [
+                "Authorization": "Bearer " + NetworkConst.apikey,
+                "Accept": "application/json"
+            ]
+            
+            var url = NetworkConst.base_url
+            url = url + "quotes/?filter=" + pseudo + "&type=user"
+            AF.request(url as URLConvertible, method: .get, encoding: JSONEncoding.default, headers: headers).responseJSON { response in
+                switch response.result {
+                case let .success(value):
+                    
+                    listener.handleJSON(json: JSON(value))
+                    
+                case let .failure(error):
+                    print(error)
+                }
+            }
+        }
+        
+        public static func getFavQuotesbyUser(pseudo: String, listener: QuotesByUserNetwortProtocol) {
+            let headers: HTTPHeaders = [
+                "Authorization": "Bearer " + NetworkConst.apikey,
+                "Accept": "application/json"
+            ]
+            
+            var url = NetworkConst.base_url
+            url = url + "quotes/?filter=" + pseudo + "&type=user"
+            AF.request(url as URLConvertible, method: .get, encoding: JSONEncoding.default, headers: headers).responseJSON { response in
+                switch response.result {
+                case let .success(value):
+                    
+                    listener.handleJSON(json: JSON(value))
+                    
+                case let .failure(error):
+                    print(error)
+                }
+            }
+        }
 }
